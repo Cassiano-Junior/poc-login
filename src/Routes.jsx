a199dbef5e14be0ea4996f61dc31e7d2ff5f6ba5
Cassiano Junior <cassiano.junior@hotmail.com>
2025-06-02 23:19:43 -0300

    work in progress

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,4 +1,4 @@
-import {RouterProvider, createBrowserRouter} from 'react-router'
+import {RouterProvider, createBrowserRouter} from 'react-router-dom'
 import {Login} from './pages/Login'
 import {Dashboard} from './pages/Dashboard'
 import { Navigate } from 'react-router';
